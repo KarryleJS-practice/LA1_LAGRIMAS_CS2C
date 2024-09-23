0929a7f9bcc2f24e9bb2599f904a52dfe386dd0e
KarryleJS-practice <karrylelagrimas6@gmail.com>
2024-09-23 09:01:17 +0800

    Create LA1_LAGRIMAS_CS2C.js

---
+++ LA1_LAGRIMAS_CS2C.js
@@ -0,0 +1,10 @@
+// Variables
+let name = "Karryle Banasan Lagrimas";
+let whatDoYouWannaBecomeInYourLife = "Data Analyst";
+let gender = "Female";
+let = facebookAcct = "Karryle Banasan Lagrimas";
+
+console.log(name);
+console.log(whatDoYouWannaBecomeInYourLife);
+console.log(gender);
+console.log(facebookAcct);
